fdff5a2f3bfe29d231c35b09b000fb56fb4de3aa
jbutler606 <j.butler7971@gmail.com>
2022-05-13 12:04:00 -0500

    final commit for submission

--- FileSys.py
+++ FileSys.py
@@ -44,12 +44,17 @@ class Directory(Node):
         self.dir_contents.append((item, item.identifier))
 
 def search(target_dir, dir):
+    # Very basic linear search function. Was originally used to recursively Depth First Search the file system
+    # See below for more explanation.
     global current_dir
 
     # Base case if the target directory is the current directory that we're in
-    if dir.name == target_dir:
-        current_dir = dir
-        return True
+    # Deprecated as it was part of the original recursive method. This would not
+    # provide correct results if there was a directory that had the same name as it's parent
+
+    #if dir.name == target_dir:
+    #    current_dir = dir
+    #    return True
     
     # Check contents of directory for the requested directory
     for dir_obj in dir.dir_contents:
@@ -60,10 +65,12 @@ def search(target_dir, dir):
                 current_dir = obj
                 return True
     # Deprecating the below code as it was used to recursively search for a directory.
-    # For the purposes of "cd" this is not necessary. It may be necessary in the future
-    # as part of a function to search possible files/directories as part of a query
-    #        elif dir_child.name != target_dir:
-    #            if search(target_dir, dir_child):
+    # For the basic purposes of "cd" this is not necessary. It may be necessary in the future
+    # as part of a function to search possible files/directories as part of a query or to
+    # make the searching function more robust.
+
+    #        elif obj.name != target_dir:
+    #            if search(target_dir, obj):
     #                return True
 
     # We did not find the requested directory.
@@ -76,22 +83,20 @@ def function_ls():
         print(dir_obj[0])
 
 def function_cd(target_dir, dir):
+    # Changes current directory based on the given target. Will fail if target not found.
     global current_dir
 
     if target_dir == "..":
+        # User wants to go to the parent_dir
         current_dir = current_dir.parent_dir
         return True
 
     if target_dir == "~":
+        # User wants to go back to the root
         current_dir = root
         return True
     
-    if not search(target_dir, dir):
-        print("Cannot find directory", target_dir,"because it does not exist.")
-        return False
-    else:
-        return True
-
+    return search(target_dir, dir)
 
 def function_mkdir(name):
     # Creates new directory within the current directory
@@ -115,44 +120,56 @@ def function_touch(file_name):
         print("File not created as a file with the same name already exists in the current directory.")
 
 def main():
-    # Create root directory as the current and only directory within the filesystem on program start.
     global root
     global current_dir
-    
+
+    # Create root directory as the current and only directory within the filesystem on program start.
+    # There should be a better way to initialize root as the parent_dir of root. The reason this is done
+    # is typically the root folder points to itself when "cd .." is done.
     root = Directory("root")
+    root.parent_dir = root
     current_dir = root
+
+    # Run the logic using user input. These functions only support 1 argument at this time as denoted within the README.md
     while(True):
         cmd = input().split(" ")
         function = cmd[0]
         if function == "ls":
             function_ls()
         elif function == "mkdir":
-            if len(cmd) > 1:
+            if len(cmd) == 2:
                 function_mkdir(cmd[1])
             else:
-                print("No parameter was given. Function was not run.")
+                print("Incorrect parameter(s) provided. Function was not run.")
         elif function == "cd":
-            if len(cmd) > 1:
+            if len(cmd) == 2:
                 path_list = cmd[1].split(sep = "/")
                 if len(path_list) >= 1:
+                    # Splits input into singular strings to essentially run cd from the "current_dir" for as much directories as there were in the path
+                    # Example: "cd test/test2" would run "cd test" and then "cd test2". In "cd test2", the current directory would be "test" providing that it existed.
                     if path_list[0] == '':
                         # This means that the first part of the path following the command cd was '/' indicating an absolute path starting at the root.
                         path_list[0] = "~"
+                # pending_dir saves the current_dir in case the file path does not exist.
                 pending_dir = current_dir
                 cmd_success = True
                 for path in path_list:
+                    # Execute cd for each path as exampled above.
                     cmd_success = function_cd(path, current_dir)
                 if cmd_success:
+                    # Update timestamp. current_dir is already updated to what the last item in the path given was.
                     current_dir.access_timestamp = datetime.datetime.now()
                 else:
+                    # Path given does not exist. Revert current_dir to original "current_dir"
                     current_dir = pending_dir
+                    print("Cannot find directory", cmd[1] ,"because it does not exist.")
             else:
-                print("No parameter was given. Function was not run.")
+                print("Incorrect parameter(s) provided. Function was not run.")
         elif function == "touch":
-            if len(cmd) > 1:
+            if len(cmd) == 2:
                 function_touch(cmd[1])
             else:
-                print("No parameter was given. Function was not run.")
+                print("Incorrect parameter(s) provided. Function was not run.")
         elif function == "exit":
             break
         else:
--- README.md
+++ README.md
@@ -1,8 +1,140 @@
-This file system is extremely basic and each function currently only performs the most default form of the function as seen in a typical linux shell.
+This file system is extremely basic and each function currently only performs the most default form of the function as seen in a typical linux shell. There are a lot of improvements and optimizations that can be made.
+For the purposes of this exercise, the current functionality seems appropriate.
 
 ls    - Currently only lists out the files/directories within the current directory that's being accessed.
 touch - Currently only creates a new file within the current directory that's being accessed.
         This will only succeed if there is not already a file with that name (caps insensitive) present in the current directory.
 mkdir - Currently only supports the creation of a directory within the current directory. Function will only take 1 parameter
         which is the name. If a directory path is provided, it will simply be set as the name of the directory created and will not actually create a directory at that path.
-cd    - This function is slightly 
+cd    - This function is slightly more involved than the previous functions. It will change directories using the current directory as a baseline for searching. However, it
+        also is able to parse a path to traverse down the file system and change to a directory outside the current directory. cd currently supports '~' for the root directory
+        and '..' for the previous directory. This parsing could theoretically be used for the other functions as well to provide further functionality to them in the future.
+
+OUTPUT:
+
+ls
+mkdir 1
+mkdir 2
+cd 1
+mkdir 1-1
+cd ..
+ls   
+1       Fri May 13 11:38:07 2022        Directory
+2       Fri May 13 11:38:06 2022        Directory
+cd 2
+mkdir 2-1
+mkdir 2-2
+ls
+2-1     Fri May 13 11:38:38 2022        Directory
+2-2     Fri May 13 11:38:39 2022        Directory
+cd ..
+touch 1
+ls
+1       Fri May 13 11:38:07 2022        Directory
+2       Fri May 13 11:38:34 2022        Directory
+1       Fri May 13 11:38:59 2022        File
+cd 1
+cd ..
+ls
+1       Fri May 13 11:41:44 2022        Directory
+2       Fri May 13 11:38:34 2022        Directory
+1       Fri May 13 11:38:59 2022        File
+cd 1/1-1
+ls
+cd ..
+ls
+1-1     Fri May 13 11:42:01 2022        Directory
+cd ..
+ls
+1       Fri May 13 11:42:06 2022        Directory
+2       Fri May 13 11:38:34 2022        Directory
+1       Fri May 13 11:38:59 2022        File
+cd 2/2-1
+mkdir 2-1-1
+cd ~
+cd 2/2-1/2-1-1
+ls
+cd .
+Cannot find directory . because it does not exist.
+cd ..
+ls
+2-1-1   Fri May 13 11:42:45 2022        Directory
+cd ..
+ls
+2-1     Fri May 13 11:42:50 2022        Directory
+2-2     Fri May 13 11:38:39 2022        Directory
+cd ..
+ls
+1       Fri May 13 11:42:06 2022        Directory
+2       Fri May 13 11:42:53 2022        Directory
+1       Fri May 13 11:38:59 2022        File
+cd 3
+Cannot find directory 3 because it does not exist.
+cd 1/1-2
+Cannot find directory 1/1-2 because it does not exist.
+cd ../../../..
+ls
+1       Fri May 13 11:42:06 2022        Directory
+2       Fri May 13 11:42:53 2022        Directory
+1       Fri May 13 11:38:59 2022        File
+mkdir 1
+Directory not created as a directory with the same name already exists in the current directory.
+touch 1
+File not created as a file with the same name already exists in the current directory.
+cd 1
+ls
+1-1     Fri May 13 11:42:01 2022        Directory
+cd /2/2-1
+ls
+2-1-1   Fri May 13 11:42:45 2022        Directory
+
+If you'd like to run this test, here are the command line by line that can be thrown into a bash file and piped as user input:
+ls
+mkdir 1
+mkdir 2
+cd 1
+mkdir 1-1
+cd ..
+ls 
+cd 2
+mkdir 2-1
+mkdir 2-2
+ls
+cd ..
+touch 1
+ls
+cd 1
+cd ..
+ls
+cd 1/1-1
+ls
+cd ..
+ls
+cd ..
+ls
+cd 2/2-1
+mkdir 2-1-1
+cd ~
+cd 2/2-1/2-1-1
+ls
+cd .
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+cd 3
+cd 1/1-2
+cd ../../../..
+ls
+mkdir 1
+touch 1
+cd ~
+cd 1
+ls
+cd /2/2-1
+
+This test demonstrates everything that the basic filesystem can do. It shows that touch creates a file and mkdir creates a directory within the current directory. It also shows that there can be a file and directory with the same name, but that the file system
+errors when it tries to create a new file/directory with the same name of one that already exists within the current_directory. It also displays ls and the information that it shows (which is a little more than the traditional ls in linux shows by default).
+I wanted ls to display more information for demo purposes. This also shows that cd has the functionality to go up to a parent_directory as well as return to the root. It also shows (near the end of the demo output) that the filesystem can take the absolute path.
